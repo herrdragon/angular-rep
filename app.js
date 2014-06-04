291a9f644a636be88b2ab2958716d8963b7899a4
mherrnsdorf <migherr@hotmail.com>
2014-06-03 23:54:19 -0400

    Added City and State separate.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 function Hello($scope, $http) {
-	var state = 'FL';
   $scope.getAddress = function() {
     $scope.userAddress = document.getElementById("userAddress").value;
+    $scope.city = document.getElementById("city").value;
+    $scope.state = document.getElementById("state").value;
 		$http.post('https://www.googleapis.com/civicinfo/us_v1/representatives/lookup?key=AIzaSyCN9rkEJ848kuw9-YO7vZ41Mt7v2bhckcs'
-        	,{'address': $scope.userAddress+' '+state}).
+        	,{'address': $scope.userAddress+' '+$scope.city+' '+$scope.state}).
         		success(function(data) {
             	$scope.greeting = data;
         });
