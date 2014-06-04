ebdc5da3c07c943ce071e01d6e24f6f64e5d9762
mherrnsdorf <migherr@hotmail.com>
2014-06-03 23:35:06 -0400

    Get address from text box.  State is added on code

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 function Hello($scope, $http) {
-	$scope.userAddress = document.getElementById("userAddress").value;
-      $scope.getAddress = function(){
-$http.post('https://www.googleapis.com/civicinfo/us_v1/representatives/lookup?key=AIzaSyCN9rkEJ848kuw9-YO7vZ41Mt7v2bhckcs'
-               , {$scope.userAddress }).
-        success(function(data) {
-            $scope.greeting = data;
+	var state = 'FL';
+  $scope.getAddress = function() {
+    $scope.userAddress = document.getElementById("userAddress").value;
+		$http.post('https://www.googleapis.com/civicinfo/us_v1/representatives/lookup?key=AIzaSyCN9rkEJ848kuw9-YO7vZ41Mt7v2bhckcs'
+        	,{'address': $scope.userAddress+' '+state}).
+        		success(function(data) {
+            	$scope.greeting = data;
         });
-};  
-}
-
-
-
-// function getAddress () {
-// 	$scope.address= alert("hi");
-//     $scope.userAddress = document.getElementById("userAddress").value;'address': '1205 Mariposa Ave Miami FL'
-// };
+};
+};
