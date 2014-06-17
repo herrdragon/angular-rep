be936b55cb40b547456b286d55b698fe74315a63
mherrnsdorf <migherr@hotmail.com>
2014-06-16 20:06:10 -0400

    added leaflets map can

--- app.js
+++ app.js
@@ -9,41 +9,4 @@ function callApi($scope, $http) {
             	$scope.result = true;
         });
 	};
-
-		$scope.geolocateUser = function() {
-		if (navigator.geolocation) {
-		    navigator.geolocation.getCurrentPosition(function(position) {
-		        var pos = (position.coords.latitude, position.coords.longitude);
-		        writeAddressName(pos); //comment this line to stop the page from reload when debugging 
-		    });
-		} 
-		else  {
-		    alert('Error: The Geolocation service failed.');
-		}
-		};
-
-		$scope.writeAddressName = function(latLng){
-	    var geocoder = new google.maps.Geocoder();
-	    console.log(gepcoder);
-	    geocoder.geocode({
-	      "location": latLng
-	    }, function(results, status) {
-	    if (status === google.maps.GeocoderStatus.OK){
-	        var field = results[0];
-	        var component = field.address_components;
-	        var len = component.length;
-	        var addr = 'error';
-	        for ( var i = 0; i < len; ++i ) {
-	            if (component[i].long_name === 'Miami-Dade County') {
-	                addr = field.formatted_address;
-	                alert(addr);
-	                break;
-	            }
-	        }
-	        getAddress(addr);
-	    } 
-	    else  
-	        alert('Location search was unsuccesful. Error : ' + status);
-	});
-	};
 };
