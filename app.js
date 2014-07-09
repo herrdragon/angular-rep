463736987b21df25829742463778e56d6021c674
mherrnsdorf <migherr@hotmail.com>
2014-07-08 22:01:39 -0400

    Clean extra code - added lodash script for debugging - ngrepeat for shorter html code

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-// function callApi($scope, $http) {
-//   $scope.getAddress = function() {
-//     $scope.userAddress = document.getElementById("userAddress").value;
-//     $scope.city = document.getElementById("city").value;
-//     $scope.state = "FL";
-// 		$http.post('https://www.googleapis.com/civicinfo/us_v1/representatives/lookup?key=AIzaSyCN9rkEJ848kuw9-YO7vZ41Mt7v2bhckcs'
-//         	,{'address': $scope.userAddress+' '+$scope.city+' '+$scope.state}).success(function(data) {
-//             	$scope.greeting = data;
-//             	$scope.result = true;
-//         });
-// 	};
-// };
-
-// (function () {
-    // you could do something like
-    // angular.module('RepMiami', []);
-    //        .controller('MainCtrl', ['$scope', function($scope){}]);
-    // or 
     var repmiami = angular.module('RepMiami', []);
     repmiami.controller('MainCtrl', ['$scope', function($scope){}]);
 
@@ -34,57 +16,62 @@
 
     function callApi($scope, $http) {
       $scope.getAddress = function() {
-        $scope.userAddress = document.getElementById("userAddress").value;
-        $scope.city = document.getElementById("city").value;
+        $scope.userAddress = userAddress.value;
+        $scope.city = city.value;
         $scope.state = "FL";
             $http.post('https://www.googleapis.com/civicinfo/us_v1/representatives/lookup?key=AIzaSyCN9rkEJ848kuw9-YO7vZ41Mt7v2bhckcs'
                 ,{'address': $scope.userAddress+' '+$scope.city+' '+$scope.state}).success(function(data) {
                     $scope.greeting = data;
+
+                    _.forEach($scope.greeting.divisions, function(official) {
+                        console.log(official);
+                    })
+
                     var dade = "Miami-Dade County"
                     var county = $scope.greeting.divisions;
-                    if(county[Object.keys(county)[2]].name === dade){
+                    if(county[Object.keys(county)[1]].name === dade){
                         $scope.result = true;
                     }else{
                         alert('You are address is not in '+dade)
+                        $scope.greeting = '';
                     }
             });
         };
-        
-            $scope.geolocateUser = function() {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function(position) {
-                    var pos = (position.coords.latitude, position.coords.longitude);
-                    writeAddressName(pos); //comment this line to stop the page from reload when debugging 
-                });
-            } 
-            else  {
-                alert('Error: The Geolocation service failed.');
-            }
-            };
+            //geolocation start here
+            // $scope.geolocateUser = function() {
+            // if (navigator.geolocation) {
+            //     navigator.geolocation.getCurrentPosition(function(position) {
+            //         var pos = (position.coords.latitude, position.coords.longitude);
+            //         writeAddressName(pos);
+            //     });
+            // } 
+            // else  {
+            //     alert('Error: The Geolocation service failed.');
+            // }
+            // };
 
-            $scope.writeAddressName = function(latLng){
-            var geocoder = new google.maps.Geocoder();
-            console.log(gepcoder);
-            geocoder.geocode({
-              "location": latLng
-            }, function(results, status) {
-            if (status === google.maps.GeocoderStatus.OK){
-                var field = results[0];
-                var component = field.address_components;
-                var len = component.length;
-                var addr = 'error';
-                for ( var i = 0; i < len; ++i ) {
-                    if (component[i].long_name === 'Miami-Dade County') {
-                        addr = field.formatted_address;
-                        alert(addr);
-                        break;
-                    }
-                }
-                getAddress(addr);
-            } 
-            else  
-                alert('Location search was unsuccesful. Error : ' + status);
-        });
-        };
+        //     $scope.writeAddressName = function(latLng){
+        //     var geocoder = new google.maps.Geocoder();
+        //     console.log(gepcoder);
+        //     geocoder.geocode({
+        //       "location": latLng
+        //     }, function(results, status) {
+        //     if (status === google.maps.GeocoderStatus.OK){
+        //         var field = results[0];
+        //         var component = field.address_components;
+        //         var len = component.length;
+        //         var addr = 'error';
+        //         for ( var i = 0; i < len; ++i ) {
+        //             if (component[i].long_name === 'Miami-Dade County') {
+        //                 addr = field.formatted_address;
+        //                 alert(addr);
+        //                 break;
+        //             }
+        //         }
+        //         getAddress(addr);
+        //     } 
+        //     else  
+        //         alert('Location search was unsuccesful. Error : ' + status);
+        // });
+        // };
     };
-// })();
