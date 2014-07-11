ebf5d31ca13ff9eac63fffa03d3c463edd8892f6
mherrnsdorf <migherr@hotmail.com>
2014-07-11 14:13:30 -0400

    Committed

--- app.js
+++ app.js
@@ -22,19 +22,16 @@
             $http.post('https://www.googleapis.com/civicinfo/us_v1/representatives/lookup?key=AIzaSyCN9rkEJ848kuw9-YO7vZ41Mt7v2bhckcs'
                 ,{'address': $scope.userAddress+' '+$scope.city+' '+$scope.state}).success(function(data) {
                     $scope.greeting = data;
-
-                    _.forEach($scope.greeting.divisions, function(official) {
-                        console.log(official);
-                    })
-
-                    var dade = "Miami-Dade County"
-                    var county = $scope.greeting.divisions;
-                    if(county[Object.keys(county)[1]].name === dade){
+                    var countywide = _.filter($scope.greeting.divisions, {'scope':'countywide'});
+                    _.forEach(countywide, function(county) {
+                        // console.log(county.name);
+                    if(county.name === 'Miami-Dade County'){
                         $scope.result = true;
                     }else{
-                        alert('You are address is not in '+dade)
+                        alert('You are address is in '+county.name+', not in Miami-Dade County')
                         $scope.greeting = '';
                     }
+                })
             });
         };
             //geolocation start here
